ee7e6917cc2cd6df551ec835cf38add5bfcea348
Michael Thompson <thompsonmichaelj2020@gmail.com>
2022-10-17 18:11:40 -0400

    Created DisplayEntries Component and Instantiated Component on App Component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
 import React, { useState } from 'react';
-
+import DisplayEntries from './Components/DisplayEntries';
 
 function App() {
 
-  const [entries, setEntries] = useState([{weight: 175, date: '11-23-2021'}])//setting the default value; future projects, will leave string empty for interactions with DB
+  const [entries, setEntries] = useState([{ weight: 175, date: '11-23-2021' }, { weight: 177, date: '11-24-2021' }, { weight: 172, date: '11-25-2021' }])//setting the default value; future projects, will leave string empty for interactions with DB
 
   return (
     <div>
-      <table>
-        <thead>
-          <tr>
-            <th>Entry Number</th>
-            <th>Weight</th>
-            <th>Date</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>175</td>
-            <td>11-23-2021</td>
-          </tr>
-        </tbody>
-      </table>
+      <DisplayEntries parentEntries={entries} />
     </div>
   );
 }
